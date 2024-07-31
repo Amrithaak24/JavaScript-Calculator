726d64eb17ac25a50c2c62b217185e2d9fb51b51
Amrithaak24 <amrithaaakk199@gmail.com>
2024-07-31 12:02:10 +0530

    Add files via upload

---
+++ calculator/calculator.js
@@ -0,0 +1,15 @@
+function buttonClick(val)
+{
+    document.getElementById("screen").value=
+    document.getElementById("screen").value+val;
+}
+function clearDisplay()
+{
+    document.getElementById("screen").value="";
+}
+ function equalClick()
+ {
+let text= document.getElementById("screen").value;
+let result =eval(text);
+document.getElementById("screen").value=result;
+ }
